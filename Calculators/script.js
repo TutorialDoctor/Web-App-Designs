795a52a122ce6e9ac43b597db4482114fbe44349
Tutorial Doctor <thetutorialdoctor@gmail.com>
2018-01-29 10:12:57 -0500

    Fuzzy logic
    Fuzzy logic calculator.

--- Calculators/script.js
+++ Calculators/script.js
@@ -1,51 +1,61 @@
 function add(){
-  x = Number(document.getElementById("x").value);
-  y = Number(document.getElementById("y").value);
+  var x = Number(document.getElementById("x").value);
+  var y = Number(document.getElementById("y").value);
   document.getElementById("output").innerHTML=x+y
 }
 
 function subtract(){
-  x = Number(document.getElementById("x").value);
-  y = Number(document.getElementById("y").value);
+  var x = Number(document.getElementById("x").value);
+  var y = Number(document.getElementById("y").value);
   document.getElementById("output").innerHTML=x-y
 }
 
 function multiply(){
-  x = Number(document.getElementById("x").value);
-  y = Number(document.getElementById("y").value);
+  var x = Number(document.getElementById("x").value);
+  var y = Number(document.getElementById("y").value);
   document.getElementById("output").innerHTML=x*y
 }
 
 function divide(){
-  x = Number(document.getElementById("x").value);
-  y = Number(document.getElementById("y").value);
+  var x = Number(document.getElementById("x").value);
+  var y = Number(document.getElementById("y").value);
   document.getElementById("output").innerHTML=x/y
 }
 
 function remainder(){
-  x = Number(document.getElementById("x").value);
-  y = Number(document.getElementById("y").value);
+  var x = Number(document.getElementById("x").value);
+  var y = Number(document.getElementById("y").value);
   document.getElementById("output").innerHTML=x%y
 }
 
 function power(){
-  x = Number(document.getElementById("x").value);
-  y = Number(document.getElementById("y").value);
+  var x = Number(document.getElementById("x").value);
+  var y = Number(document.getElementById("y").value);
   document.getElementById("output").innerHTML=x**y
 }
 
 function concat(){
-  x = document.getElementById("s1").value;
-  y = document.getElementById("s2").value;
+  var x = document.getElementById("s1").value;
+  var y = document.getElementById("s2").value;
   document.getElementById("concatch").innerHTML=x+" "+y
 }
 
 function dToC(){
-  temp = document.getElementById("temp").value;
+  var temp = document.getElementById("temp").value;
   document.getElementById("temperature").innerHTML=(temp-32)*.5556
 }
 
 function cToD(){
-  temp = document.getElementById("temp").value;
+  var temp = document.getElementById("temp").value;
   document.getElementById("temperature").innerHTML=(temp*1.8)+32
+}
+
+function membership(){
+  var min = parseInt(document.getElementById("min").value,10);
+  var val = parseInt(document.getElementById("val").value,10);
+  var max = parseInt(document.getElementById("max").value,10);
+  var top = val-min
+  var bottom = max-min
+  var M = top/bottom
+  document.getElementById("membership").innerHTML=M
 }
